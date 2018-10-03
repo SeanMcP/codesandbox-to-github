ed68c29cee9541ce80960627b5d94a2e4927b29d
Sean McPherson <seanmcp25@gmail.com>
2018-10-03 09:38:06 -0400

    Declare love

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import styled from 'styled-components'
 
-import './styles.css';
+import './styles.css'
+
+const Container = styled.div`
+	background: #fff;
+	border-radius: 0.5em;
+	box-shadow: 0 0.125em 0.25em rgba(0, 0, 0, 0.125);
+	margin: 1em;
+	padding: 1em 2em;
+`
+
+const Heading = styled.h1`
+	text-align: center;
+`
 
 const App = () => (
-	<div className="App">
-		<h1>Hello CodeSandbox</h1>
-		<h2>Start editing to see some magic happen!</h2>
-	</div>
-);
+	<Container>
+		<Heading>
+			CodeSandbox{' '}
+			<span role="img" aria-label="heart">
+				❤️
+			</span>{' '}
+			GitHub
+		</Heading>
+	</Container>
+)
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
